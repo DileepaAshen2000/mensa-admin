4e81bc8f07ed682a045ccad6054d26b8e29004e3
Dileepa Ashen <157355223+DileepaAshen2000@users.noreply.github.com>
2025-06-12 15:00:14 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 // for local development
-export const BASE_URL = 'http://localhost:8000/api';
+// export const BASE_URL = 'http://localhost:8000/api';
 
 // for deployment
-// export const BASE_URL = 'http://localhost:8000/api';
+export const BASE_URL = 'https://5d98-223-224-31-140.ngrok-free.app';
